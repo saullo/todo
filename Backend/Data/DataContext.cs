12a5e09b9646405c9259143158237d7c45ea7a67
Saullo Bretas Silva <saullo.silva303@gmail.com>
2022-04-03 18:55:10 -0300

    Backend: Added DataContext using InMemoryDatabase

---
+++ Backend/Data/DataContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+    }
+}
